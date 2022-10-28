0cb68dc4506e2ec6646615f436fdbcd7179170f9
diogovelho <diogovelho@gmail.com>
2022-10-28 13:28:50 +0100

    trying to simplify roles

---
+++ product/service/lambda-services/cognito/signup/src/main/java/cloud/minka/cognito/signup/repository/TenantRepository.java
@@ -0,0 +1,51 @@
+package cloud.minka.cognito.signup.repository;
+
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.Map;
+
+import static cloud.minka.cognito.signup.model.cloudformation.TenantStatus.PENDING_CONFIGURATION;
+
+@ApplicationScoped
+public class TenantRepository {
+    @Inject
+    DynamoDbClient client;
+    public void createTenantTable(String tenantTable) {
+        //Create the tenant on dynamodb
+        try {
+            client.createTable(tableRequest ->
+                    tableRequest.tableName(tenantTable)
+                            .keySchema(keySchema -> keySchema.attributeName("PK").keyType(KeyType.HASH))
+                            .attributeDefinitions(attrDef -> attrDef.attributeName("PK").attributeType(ScalarAttributeType.S))
+                            .provisionedThroughput(throughput -> throughput.writeCapacityUnits(1L).readCapacityUnits(1L)));
+        } catch (ResourceInUseException e) {
+            System.out.println("event::cognito::signup::request::tenant::table::exists:" + e.getMessage());
+        }
+
+    }
+
+    public void insertTenantIntoTable(String tenantTable, String tenantId) {
+        //Insert the tenant on dynamodb
+        AttributeValue pk = AttributeValue.builder().s(tenantId).build();
+        AttributeValue sk = AttributeValue.builder().s("tenant").build();
+        AttributeValue status = AttributeValue.builder().s(String.valueOf(PENDING_CONFIGURATION)).build();
+        PutItemRequest request = PutItemRequest.builder()
+                .tableName(tenantTable)
+                .item(Map.of("PK", pk, "SK", sk, "status", status))
+                .build();
+        client.putItem(request);
+    }
+
+    public GetItemResponse getTenantFromTable(String tenantTable, String tenantId) {
+        //Get the tenant from dynamodb
+        AttributeValue pk = AttributeValue.builder().s(tenantId).build();
+        GetItemRequest request = GetItemRequest.builder()
+                .tableName(tenantTable)
+                .key(Map.of("PK", pk))
+                .build();
+        return client.getItem(request);
+    }
+}
--- product/service/lambda-services/cognito/signup/src/main/java/cloud/minka/cognito/signup/service/PreSignupProcessingService.java
+++ product/service/lambda-services/cognito/signup/src/main/java/cloud/minka/cognito/signup/service/PreSignupProcessingService.java
@@ -3,22 +3,19 @@ package cloud.minka.cognito.signup.service;
 import cloud.minka.cognito.signup.model.cloudformation.CognitoSignupEvent;
 import cloud.minka.cognito.signup.model.cloudformation.ResponseSignup;
 import cloud.minka.cognito.signup.model.cloudformation.TenantStatus;
-import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.model.*;
+import cloud.minka.cognito.signup.repository.TenantRepository;
+import software.amazon.awssdk.services.dynamodb.model.GetItemResponse;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.util.Map;
 
-import static cloud.minka.cognito.signup.model.cloudformation.TenantStatus.PENDING_CONFIGURATION;
 
 @ApplicationScoped
 public final class PreSignupProcessingService {
+
+
     @Inject
-    DynamoDbClient client;
-
-
+    TenantRepository tenantRepository;
 
     /**
      * Process pre signup response signup.
@@ -30,11 +27,11 @@ public final class PreSignupProcessingService {
      */
     public CognitoSignupEvent process(CognitoSignupEvent input) {
         String tableName = "tenant";
-        createTenantTable(tableName);
+        tenantRepository.createTenantTable(tableName);
         String userEmail =  input.request().get("userAttributes").get("email").asText();
         String tenantDomain = userEmail.split("@")[1];
         System.out.println("event::cognito::signup::request::tenant::domain:" + tenantDomain);
-        GetItemResponse tenant = getTenantFromTable(tableName, tenantDomain);
+        GetItemResponse tenant = tenantRepository.getTenantFromTable(tableName, tenantDomain);
         System.out.println("event::cognito::signup::request::tenant::response:" + tenant);
         CognitoSignupEvent responseSuccess = new CognitoSignupEvent(
                 input.version(),
@@ -47,7 +44,7 @@ public final class PreSignupProcessingService {
                 new ResponseSignup("true", "true", "true")
         );
         if (tenant.item().size() == 0) {
-            insertTenantIntoTable(tableName, tenantDomain);
+            tenantRepository.insertTenantIntoTable(tableName, tenantDomain);
             return responseSuccess;
         }
         //Check if the tenant is in pending configuration
@@ -55,48 +52,14 @@ public final class PreSignupProcessingService {
         TenantStatus tenantStatus = TenantStatus.valueOf(tenant.item().get("status").s());
         return switch (tenantStatus) {
             case PENDING_CONFIGURATION ->
-                    throw new IllegalArgumentException("You domain exists but is not yet fully configured. Please contact the person responsible for your Organization.");
+                    throw new IllegalArgumentException("Your domain exists but is not yet fully configured. Please contact the person responsible for your Organization.");
             case ACTIVE -> responseSuccess;
             default ->
-                    throw new IllegalArgumentException("You domain exists but is not yet fully configured. Please contact the person responsible for your Organization.");
+                    throw new IllegalArgumentException("The tenant is not in a valid state");
         };
     }
 
-    public void createTenantTable(String tenantTable) {
-        //Create the tenant on dynamodb
-        try {
-            client.createTable(tableRequest ->
-                    tableRequest.tableName(tenantTable)
-                            .keySchema(keySchema -> keySchema.attributeName("PK").keyType(KeyType.HASH))
-                            .attributeDefinitions(attrDef -> attrDef.attributeName("PK").attributeType(ScalarAttributeType.S))
-                            .provisionedThroughput(throughput -> throughput.writeCapacityUnits(1L).readCapacityUnits(1L)));
-        } catch (ResourceInUseException e) {
-            System.out.println("event::cognito::signup::request::tenant::table::exists:" + e.getMessage());
-        }
 
-    }
-
-    public void insertTenantIntoTable(String tenantTable, String tenantId) {
-        //Insert the tenant on dynamodb
-        AttributeValue pk = AttributeValue.builder().s(tenantId).build();
-        AttributeValue sk = AttributeValue.builder().s("tenant").build();
-        AttributeValue status = AttributeValue.builder().s(String.valueOf(PENDING_CONFIGURATION)).build();
-        PutItemRequest request = PutItemRequest.builder()
-                .tableName(tenantTable)
-                .item(Map.of("PK", pk, "SK", sk, "status", status))
-                .build();
-        client.putItem(request);
-    }
-
-    public GetItemResponse getTenantFromTable(String tenantTable, String tenantId) {
-        //Get the tenant from dynamodb
-        AttributeValue pk = AttributeValue.builder().s(tenantId).build();
-        GetItemRequest request = GetItemRequest.builder()
-                .tableName(tenantTable)
-                .key(Map.of("PK", pk))
-                .build();
-        return client.getItem(request);
-    }
 }
 
 
