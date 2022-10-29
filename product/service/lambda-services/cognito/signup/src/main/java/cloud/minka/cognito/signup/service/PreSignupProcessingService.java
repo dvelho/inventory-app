c2fc5eb952a56cf9f80a4b4158669a7bc369f71e
diogovelho <diogovelho@gmail.com>
2022-10-29 19:25:19 +0100

    Adding testing

--- product/service/lambda-services/cognito/signup/src/main/java/cloud/minka/cognito/signup/service/PreSignupProcessingService.java
+++ product/service/lambda-services/cognito/signup/src/main/java/cloud/minka/cognito/signup/service/PreSignupProcessingService.java
@@ -5,6 +5,7 @@ import cloud.minka.cognito.signup.model.cloudformation.ResponseSignup;
 import cloud.minka.cognito.signup.model.cloudformation.TenantStatus;
 import cloud.minka.cognito.signup.repository.CognitoTenantRepository;
 import cloud.minka.cognito.signup.repository.TenantRepository;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import software.amazon.awssdk.services.dynamodb.model.GetItemResponse;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -20,6 +21,8 @@ public final class PreSignupProcessingService {
 
     @Inject
     CognitoTenantRepository cognitoTenantRepository;
+    @ConfigProperty(name = "cloud.minka.tenant.table", defaultValue="dev-tenants-info-minka-cloud")
+    String tableName;
 
     /**
      * Process pre signup response signup.
@@ -30,8 +33,8 @@ public final class PreSignupProcessingService {
      * @return the response signup
      */
     public CognitoSignupEvent process(CognitoSignupEvent input) {
-        String tableName = "tenant";
-        tenantRepository.createTenantTable(tableName);
+
+      //  tenantRepository.createTenantTable(tableName);
         String userEmail = input.request().get("userAttributes").get("email").asText();
         String tenantDomain = userEmail.split("@")[1];
         System.out.println("event::cognito::signup::request::tenant::domain:" + tenantDomain);
@@ -42,11 +45,19 @@ public final class PreSignupProcessingService {
         CognitoSignupEvent responseSuccess = createSignupEvent(input);
         System.out.println("event::cognito::signup::request::tenant::response::success:" + responseSuccess);
         if (tenant.item().size() == 0) {
+            System.out.println("event::cognito::signup::request::tenant::add::group::admin");
             cognitoTenantRepository.adminAddUserToGroup(input.userPoolId(), input.userName(), "tenant.main.admin");
-            cognitoTenantRepository.createGroup(input.userPoolId(), "tenant.%s.users".formatted(tenantDomain));
+            System.out.println("event::cognito::signup::request::tenant::create::group::tenant::admin");
             cognitoTenantRepository.createGroup(input.userPoolId(), "tenant.%s.admins".formatted(tenantDomain));
+            System.out.println("event::cognito::signup::request::tenant::create::group::tenant::users");
+            cognitoTenantRepository.createGroup(input.userPoolId(), "tenant.%s.users".formatted(tenantDomain));
+            System.out.println("event::cognito::signup::request::tenant::add::group::tenant::admin");
             cognitoTenantRepository.adminAddUserToGroup(input.userPoolId(), input.userName(), "tenant.%s.admins".formatted(tenantDomain));
-            tenantRepository.updateTenant(tableName, tenantDomain, TenantStatus.ACTIVE);
+            cognitoTenantRepository.adminUpdateUserAttributes(input.userPoolId(), input.userName(), "custom:domain", tenantDomain);
+            cognitoTenantRepository.adminUpdateUserAttributes(input.userPoolId(), input.userName(), "custom:tenantId", tenantDomain);
+            cognitoTenantRepository.adminUpdateUserAttributes(input.userPoolId(), input.userName(), "custom:region", input.region());
+            System.out.println("event::cognito::signup::request::tenant::create::table::tenant::%s".formatted(tenantDomain));
+            tenantRepository.insertTenantIntoTable(tableName, tenantDomain);
             return responseSuccess;
         }
         //Check if the tenant is in pending configuration
