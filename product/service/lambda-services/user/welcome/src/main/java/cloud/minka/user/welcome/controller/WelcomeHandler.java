7d7a145c7a55c5eff7e9e664d8d8f4ea00e3d52b
diogovelho <diogovelho@gmail.com>
2022-11-02 12:13:56 +0000

    debug adding x-ray

--- product/service/lambda-services/user/welcome/src/main/java/cloud/minka/user/welcome/controller/WelcomeHandler.java
+++ product/service/lambda-services/user/welcome/src/main/java/cloud/minka/user/welcome/controller/WelcomeHandler.java
@@ -4,6 +4,8 @@ import cloud.minka.user.welcome.service.WelcomeService;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.entities.Subsegment;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
@@ -12,10 +14,12 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.Map;
 
+
 @Named("welcome-handler")
 public final class WelcomeHandler implements RequestHandler<SNSEvent, JsonNode> {
 
 
+    private static final String LAMBDA_TRACE_HEADER_PROP = "com.amazonaws.xray.traceHeader";
     ObjectMapper mapper = new ObjectMapper().registerModule(new JodaModule());
 
     WelcomeService welcomeService;
@@ -28,7 +32,16 @@ public final class WelcomeHandler implements RequestHandler<SNSEvent, JsonNode> 
 
     @Override
     public JsonNode handleRequest(SNSEvent input, Context context) {
+        String aws_region = System.getenv("AWS_REGION");
+        String x_amzn_trace_id = System.getenv("_X_AMZN_TRACE_ID");
+        System.out.println("Lambda AWS Region: " + aws_region);
+        System.out.println("Lambda _X_AMZN_TRACE_ID: " + x_amzn_trace_id);
+        String env = "--ENV--\n"
+                + "AWS_REGION: " + aws_region + "\n"
+                + "_X_AMZN_TRACE_ID: " + x_amzn_trace_id + "\n"
+                + "Trace Property: " + System.getProperty(LAMBDA_TRACE_HEADER_PROP) + "\n";
 
+        Subsegment subsegment = AWSXRay.beginSubsegment("welcome-handler");
         System.out.println("event::user::welcome::request:" + mapper.valueToTree(input));
         input.getRecords().stream().map(SNSEvent.SNSRecord::getSNS).forEach(sns -> {
             String message = sns.getMessage();
@@ -38,7 +51,7 @@ public final class WelcomeHandler implements RequestHandler<SNSEvent, JsonNode> 
             String user = messageAttributes.get("user").getValue();
             welcomeService.sendWelcomeEmail(user);
         });
-
+        AWSXRay.endSubsegment();
         return mapper.valueToTree(input);
     }
 
