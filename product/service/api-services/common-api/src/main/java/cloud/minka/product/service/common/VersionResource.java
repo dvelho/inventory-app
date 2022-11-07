de8861a864f4ee70c14edc7780e6d401940245df
diogovelho <diogovelho@gmail.com>
2022-11-07 15:42:32 +0000

    security testing

--- product/service/api-services/common-api/src/main/java/cloud/minka/product/service/common/VersionResource.java
+++ product/service/api-services/common-api/src/main/java/cloud/minka/product/service/common/VersionResource.java
@@ -36,7 +36,7 @@ public class VersionResource {
     @Path("/text")
     @Produces(MediaType.APPLICATION_JSON)
     public String versionT(String ctx) {
-        System.out.println("test: " + ctx);
+        System.out.println("tests: " + ctx);
         return this.readGitProperties();
     }
 
