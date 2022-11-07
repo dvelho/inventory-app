a23b701b4f82bb09263a5a8140088325e0161831
diogovelho <diogovelho@gmail.com>
2022-11-07 15:45:42 +0000

    security testing

--- product/service/api-services/common-api/src/main/java/cloud/minka/product/service/common/VersionResource.java
+++ product/service/api-services/common-api/src/main/java/cloud/minka/product/service/common/VersionResource.java
@@ -1,6 +1,7 @@
 package cloud.minka.product.service.common;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -32,7 +33,7 @@ public class VersionResource {
         return this.readGitProperties();
     }
 
-    @GET
+    @POST
     @Path("/text")
     @Produces(MediaType.APPLICATION_JSON)
     public String versionT(String ctx) {
